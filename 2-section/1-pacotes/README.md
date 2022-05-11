f12babf3068c6f269d1daed461bd4673604a2ee6
Wilbur <Wilbur>
2022-05-11 09:53:34 -0300

    - pacotes externos

--- 2-section/1-pacotes/README.md
+++ 2-section/1-pacotes/README.md
@@ -10,6 +10,13 @@ Ou se quiser rodar direto
 
 * go run main.go
 
+Se quiser instalar projetos/pacotes/dependencia externas
+
+* go get github.com/badoux/checkmail
+
+Se quiser limpar o go.mod caso nao esteja usando de determinada dependencia
+
+* go mod tidy
 
 
 ---
--- 2-section/1-pacotes/main.go
+++ 2-section/1-pacotes/main.go
@@ -4,9 +4,14 @@ package main
 import (
 	"modulo/auxiliar"
 	"fmt"
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Hello World")
 	auxiliar.Escrever()
+
+	// to call checkmail just insert after last slash bar
+	erro := checkmail.ValidateFormat("wilbur@test.com")
+	fmt.Println(erro)
 }
